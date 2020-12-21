2a5ec05bfd22d0fc8c6d9f5899bfc36a487e2844
Imprudent Friend <ImprudentFriend@gmail.com>
2020-12-20 23:02:48 -0500

    added html routes

---
+++ Develop/server.js
@@ -0,0 +1,27 @@
+//Sets up server
+const express = require("express");
+const app = express();
+const PORT = 8080
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log("Server listening on port 8080")
+})
+
+
+
+//Creates html paths:
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+
